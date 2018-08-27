3c00adea11ad71c907413b3781ce0f296494c722
lmm <15060335532@139.com>
2018-08-27 20:09:17 +0800

    持有对象和Movezeroes的优化

--- src/main/java/com/algorithm/MoveZeroes.java
+++ src/main/java/com/algorithm/MoveZeroes.java
@@ -1,5 +1,6 @@
 package com.algorithm;
 
+import java.lang.annotation.ElementType;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,12 +21,48 @@ public class MoveZeroes {
         for (int i=nonZeroElements.size();i<nums.length;i++)
             nums[i]=0;
     }
+    //时间复杂度O(n)
+    //空间复杂度O(1)
+    public static void moveZeroes2(int[] nums){
+        int k=0;//mums中，[0.....k]的元素均为非0元素
+        //遍历到第i个元素后，保证[0.....i]中所有非0元素都按照顺序排列在[0...k]中
+        for(int i=0;i<nums.length;i++)
+            if (nums[i]!=0)
+                nums[k++]=nums[i];
+        for (int i=k;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+
+    public static void moveZeroes3(int[] nums){
+        int k=0;//mums中，[0.....k]的元素均为非0元素
+        //遍历到第i个元素后，保证[0.....i]中所有非0元素都按照顺序排列在[0...k]中
+        //同时，[k...i]为0
+        for(int i=0;i<nums.length;i++)
+            if (nums[i]!=0)//当数组为非0数组时
+                if(i!=k)
+                swap(nums,k++,i);
+        else
+            k++;
+    }
+    //数组中的两个元素进行交换
+    public static void swap(int[] arry,int x,int y){
+        if (x<=arry.length-1&&y<=arry.length-1) {
+            int xx = arry[x];
+            int yy = arry[y];
+            arry[x] = yy;
+            arry[y] = xx;
+        }else {
+            System.out.println("数组下标超出范围！");
+        }
+    }
 
     public static void main(String[] args) {
         int arr[]={0,1,0,3,12};
         int[] target=arr;
-        moveZeroes(target);
+        //moveZeroes(target);
+        moveZeroes3(target);
         for (int i=0;i<target.length;i++)
-            System.out.println(target[i]);
+            System.out.print(target[i]+" ");
     }
 }
--- src/main/java/com/holding/ModifyingArraysAsList.java
+++ src/main/java/com/holding/ModifyingArraysAsList.java
@@ -6,13 +6,13 @@ public class ModifyingArraysAsList {
     public static void main(String[] args) {
         Random rand=new Random(47);
         Integer[] ia={1,2,3,4,5,6,7,8,9,10};
-        List<Integer> list1=new ArrayList<Integer>(Arrays.asList(ia));
+        List<Integer> list1=new ArrayList<Integer>(Arrays.asList(ia));//创建一个引用ia的元素的ArrayList，因此打乱这些应用不会修改该数组
         System.out.println("Before shuffling: "+list1);
         Collections.shuffle(list1,rand);
         System.out.println("After shuffling: "+list1);
         System.out.println("array: "+Arrays.toString(ia));
 
-        List<Integer> list2=Arrays.asList(ia);
+        List<Integer> list2=Arrays.asList(ia);//直接使用Arrays.asList的结果，就会修改ia的顺序
         System.out.println("Before shuffling: "+list2);
         Collections.shuffle(list2,rand);
         System.out.println("After shuffling: "+list2);
