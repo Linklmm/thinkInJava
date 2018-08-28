5bc51ae10f5cd9b34c6bc8babdaa5e601c3e4c7a
lmm <15060335532@139.com>
2018-08-28 20:25:48 +0800

    类型信息

---
+++ src/main/java/com/typeinfo/ClassCasts.java
@@ -0,0 +1,11 @@
+package com.typeinfo;
+class Building{}
+class House extends Building{}
+public class ClassCasts {
+    public static void main(String[] args) {
+        Building b=new House();
+        Class<House> houseType=House.class;
+        House h=houseType.cast(b);
+        h=(House)b;
+    }
+}
