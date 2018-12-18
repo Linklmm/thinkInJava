e410ef96b45064fdcee443eb0ec5c20069d9a566
lmm <1248332941@qq.com>
2018-12-18 16:03:09 +0800

    think in java

--- src/main/java/com/arrays/GeneratorsTest.java
+++ src/main/java/com/arrays/GeneratorsTest.java
@@ -14,11 +14,11 @@ public class GeneratorsTest {
     public static int size =10;
     public static void test(Class<?> surroundingClass){
         for (Class<?> type: surroundingClass.getClasses()){
-            System.out.println(type.getSimpleName()+": ");
+            System.out.print(type.getSimpleName()+" : ");
             try {
                 Generator<?> g =(Generator<?>) type.newInstance();
                 for (int i =0;i<size;i++){
-                    System.out.println(g.next()+" ");
+                    System.out.print(g.next()+" ");
                 }
                 System.out.println();
             }catch (Exception e){
---
+++ src/main/java/com/arrays/RandomGeneratorTest.java
@@ -0,0 +1,14 @@
+package com.arrays;
+
+/**
+ * @author lmm
+ * @Title: RandomGeneratorTest
+ * @ProjectName thinkInJava
+ * @Description: TODO
+ * @date 18-12-18下午3:10
+ */
+public class RandomGeneratorTest {
+    public static void main(String[] args){
+    GeneratorsTest.test(RandomGenerator.class);
+    }
+}
